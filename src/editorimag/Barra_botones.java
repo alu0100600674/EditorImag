9239403eed4b7288a83dfa3c4fec9971c580b4a5
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-10-04 20:55:22 +0100

    Cambios en la barra de botones

--- src/editorimag/Barra_botones.java
+++ src/editorimag/Barra_botones.java
@@ -17,7 +17,8 @@ public class Barra_botones extends JToolBar{
     
     private Boton Abrir;
     private Boton Guardar;
-    private Boton prueba;
+    
+    private Boton Prueba;
     
     public Barra_botones(){
         
@@ -25,6 +26,11 @@ public class Barra_botones extends JToolBar{
         this.add(Abrir);
         Guardar = new Boton("Guardar", "Icono");
         this.add(Guardar);
+        
+        this.addSeparator();
+        
+        Prueba = new Boton("Prueba", "Icono");
+        this.add(Prueba);
         
     }
     
