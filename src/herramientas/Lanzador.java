171ee7725e586123dea9c6f136e4b637e9df47da
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-10-27 16:20:32 +0000

    Creado paquete herramientas y clase Lanzador

---
+++ src/herramientas/Lanzador.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package herramientas;
+
+/**
+ *
+ * @author jonay
+ */
+public class Lanzador {
+    
+    public Lanzador(int opcion){
+        
+        switch(opcion){
+            case 1:
+                transfLinealesTramos();
+                break;
+            case 2:
+                ecualizaciónHistograma();
+                break;
+            case 3:
+                especificacionHistograma();
+                break;
+            case 4:
+                brilloYContraste();
+                break;
+            case 5:
+                correccionGamma();
+                break;
+            case 6:
+                diferenciaDosImagenes();
+                break;
+        }
+        
+    }
+    
+    private void transfLinealesTramos(){
+        
+    }
+    
+    private void ecualizaciónHistograma(){
+        
+    }
+    
+    private void especificacionHistograma(){
+        
+    }
+    
+    private void brilloYContraste(){
+        
+    }
+    
+    private void correccionGamma(){
+        
+    }
+    
+    private void diferenciaDosImagenes(){
+        
+    }
+    
+}
