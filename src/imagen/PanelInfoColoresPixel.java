e3a0069cb026e5e49aec46bd04d529fc19ac818c
Ivan Garcia <alu0100693737@ull.edu.es>
2014-10-27 17:42:39 +0100

    Creando panel informativo pixeles 2

--- src/imagen/PanelInfoColoresPixel.java
+++ src/imagen/PanelInfoColoresPixel.java
@@ -27,5 +27,38 @@ public class PanelInfoColoresPixel extends JPanel{
     private int posX = 0;
     private int posY = 0;
     
+    public int getX(){ return x;}
+    public void setX(int x) {this.x = x;}
+    
+    public int getY(){ return y;}
+    public void setY(int y) {this.y = y;}
+    
+    public int getAncho(){ return ancho;}
+    public void setAncho(int anch) { this.ancho = anch;}
+    
+    public int getAlto(){ return alto;}
+    public void setAlto(int alt) {this.alto = alt;}
+    
+    public Font getletra(){ return letra;}
+    public void setletra(Font let){ this.letra = let;}
+    
+    public int getr(){ return r;}
+    public void setr( int valor){ this.r = valor;}
+    
+    public int getg(){ return g;}
+    public void setg( int valor){ this.g = valor;}
+    
+    public int getb(){ return b;}
+    public void setb( int valor){ this.b = valor;}
+    
+    public int getPosx(){ return PosX;}
+    public void setPosx( int x){ this.posX = x;}
+    
+    public int getPosy(){ return PosY;}
+    public void setPosy( int y){this.posY = y;}
+    
+    
+            
+    
     
 }
