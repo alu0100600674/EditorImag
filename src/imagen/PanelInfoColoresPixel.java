2a47511ccae548f30316bc38f947b458fdc073b7
Ivan Garcia <alu0100693737@ull.edu.es>
2014-10-27 17:19:57 +0100

    Creando panel informativo pixeles 1

---
+++ src/imagen/PanelInfoColoresPixel.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imagen;
+
+import java.awt.Font;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author johnspirit
+ */
+public class PanelInfoColoresPixel extends JPanel{
+    //Atributos de la ventana info
+    private int x = 30;
+    private int y;
+    private int ancho = 0;
+    private int alto = 40;
+    private Font letra = new Font(Font.SANS_SERIF, Font.BOLD, 16);
+    
+    private int r = 0;
+    private int g = 0;
+    private int b = 0;
+    
+    private int posX = 0;
+    private int posY = 0;
+    
+    
+}
