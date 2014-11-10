c3f1987fcb56be3b324574e836a8814ee0571871
Ivan Garcia <alu0100693737@ull.edu.es>
2014-11-10 21:13:08 +0100

    desarrollando propiedades

--- src/imagen/Imagen.java
+++ src/imagen/Imagen.java
@@ -7,6 +7,7 @@
 package imagen;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -14,13 +15,14 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
  * @author jonay
  */
-public class Imagen {
+public class Imagen extends JFrame{
     
     private BufferedImage imgGris;
     
@@ -93,7 +95,102 @@ public class Imagen {
     }
     
     //VENTANA PROPIEDADES
+    //variables
+    private Font letra = new Font(Font.SANS_SERIF, Font.BOLD, 20);
+    private String nombre = new String("Sin nombre");
+    private String formatoImg = new String("Desconocido");
+    private int anchoImg = 0;
+    private int altoImg = 0;
+    private int min = -1;
+    private int max = -1;
+    private double entropia = 0.0;
+    private double brillo = 0.0;    
+    private double contraste = 0.0;
+	
+    private BufferedImage refBufImg;
+    
+    
+    
     public void propiedades() {
+
+	// Atributos -- referencias
+	private interfaz refVp;
+	private BufferedImage refImg;
+	
+	// Atributos de informaci�n
+	private PanelVentanaInfo texto;
+	
+	// Manejadores de atributos
+	public VentanaPrincipal getRefVp() { return refVp; }
+	public void setRefVp(VentanaPrincipal refVp) { this.refVp = refVp; }
+	
+	public BufferedImage getRefImg() { return refImg; }
+	public void setRefImg(BufferedImage refImg) { this.refImg = refImg; }
+	
+	public PanelVentanaInfo getTexto() { return texto; }
+	public void setTexto(PanelVentanaInfo texto) { this.texto = texto; }
+
+
+	public VentanaInfoImagen (VentanaPrincipal vp) {
+		super ();
+		setRefVp(vp);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		setBounds(400, 50, 600, 500);
+		setLayout(null);
+		setAlwaysOnTop(true);
+		setResizable(false);
+		setTexto(new PanelVentanaInfo(this));
+		
+		add (getTexto());
+		
+	}
+	
+	public BufferedImage copiarBufferOriginal (BufferedImage original) {
+		BufferedImage copia = new BufferedImage(original.getWidth(), original.getHeight(), original.getType());
+		for (int i = 0; i < original.getWidth(); ++i) {
+			for (int j = 0; j < original.getHeight(); ++j) {
+				copia.setRGB(i, j, original.getRGB(i, j));
+			}
+		}
+		return copia;
+	}
+	
+	public void actualizar () {
+		getTexto().setNombre(getRefVp().getGestorSubVentanas().getRefSubVentActual().getTitle());
+		getTexto().setFormatoImg(getTexto().obtenerFormato(getRefVp().getGestorSubVentanas().getRefSubVentActual().getTitle()));
+		getTexto().setAnchoImg(getRefImg().getWidth());
+		getTexto().setAltoImg(getRefImg().getHeight());
+
+		getTexto().setMin(obtenerMinGris(getRefVp().getGestorSubVentanas().getRefSubVentActual().getRefBufImg()));
+		getTexto().setMax(obtenerMaxGris(getRefVp().getGestorSubVentanas().getRefSubVentActual().getRefBufImg()));
+		
+		getRefVp().getGestorSubVentanas().getRefSubVentActual().obtenerEntropia();
+		getTexto().setEntropia(getRefVp().getGestorSubVentanas().getRefSubVentActual().getEntropia());
+		
+		getTexto().setBrillo(getRefVp().getGestorSubVentanas().getRefSubVentActual().getBrillo());
+		getTexto().setContraste(getRefVp().getGestorSubVentanas().getRefSubVentActual().getContraste());
+		getTexto().setRefBufImg(copiarBufferOriginal(getRefVp().getGestorSubVentanas().getRefSubVentActual().getRefBufImg()));
+	}
+
+	public int obtenerMinGris (BufferedImage bf) {
+		int temp = Integer.MAX_VALUE;
+		for (int i = 0; i < bf.getWidth(); ++i)
+			for (int j = 0; j < bf.getHeight(); ++j)
+				if (new Color (bf.getRGB(i, j)).getRed() < temp)
+					temp = new Color (bf.getRGB(i, j)).getRed();
+		return temp;
+	}
+	
+	public int obtenerMaxGris (BufferedImage bf) {
+		int temp = Integer.MIN_VALUE;
+		for (int i = 0; i < bf.getWidth(); ++i)
+			for (int j = 0; j < bf.getHeight(); ++j)
+				if (new Color (bf.getRGB(i, j)).getRed() > temp)
+					temp = new Color (bf.getRGB(i, j)).getRed();
+		return temp;
+	}
+	
+}
         
     /*    
         setTitle("Propiedades de la imagen");
