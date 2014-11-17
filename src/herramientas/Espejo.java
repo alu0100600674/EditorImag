a8eafaf4eba386840d6480bf29586e1bafbbeebf
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-11-17 18:07:38 +0000

    Espejo horizontal y vertical, falta probarlo

---
+++ src/herramientas/Espejo.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package herramientas;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author jonay
+ */
+public class Espejo {
+    
+    private BufferedImage img;
+    
+    public Espejo(BufferedImage tmp){
+//        img = new BufferedImage(tmp.getWidth(), tmp.getHeight(), tmp.getType());
+        img = tmp;
+    }
+    
+    public BufferedImage espejoHorizontal(){
+        BufferedImage resultado = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+        
+        int pos = img.getWidth();
+        for(int i = 0; i < img.getWidth(); i++){
+            for(int j = 0; j < img.getHeight(); j++){
+                resultado.setRGB(i, j, img.getRGB(pos, j));
+            }
+            pos--;
+        }
+        
+        return resultado;
+    }
+    
+    public BufferedImage espejoVertical(){
+        BufferedImage resultado = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+        
+        int pos = img.getHeight();
+        for(int i = 0; i < img.getHeight(); i++){
+            for(int j = 0; j < img.getWidth(); j++){
+                resultado.setRGB(i, j, img.getRGB(pos, j));
+            }
+            pos--;
+        }
+        
+        return resultado;
+    }
+    
+}
