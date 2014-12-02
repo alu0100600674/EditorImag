da2165b76c6ed8fe265d0de092af7e6d9d18480f
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-12-02 15:23:41 +0000

    diferencia entre dos imágenes funcionando, aunque lo muestra en gris

--- src/herramientas/Diferencia_dos_imagenes.java
+++ src/herramientas/Diferencia_dos_imagenes.java
@@ -6,10 +6,12 @@
 package herramientas;
 
 import static editorimag.EditorImag.gestor_img;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -27,30 +29,29 @@ public class Diferencia_dos_imagenes {
 
     public Diferencia_dos_imagenes(BufferedImage tmp1) {
         img1 = tmp1;
-        
+
         elegirImagenDos();
     }
 
     private boolean comprobarDimensiones() {
         if ((img1.getWidth() == img2.getWidth()) && (img1.getHeight() == img2.getHeight())) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    
-    private void ventanaError(){
+
+    private void ventanaError() {
         final JFrame error = new JFrame();
         error.setTitle("Error");
         error.setSize(400, 100);
         error.setResizable(false);
         error.setLocationRelativeTo(null);
-        
+
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(2, 1));
         error.add(panel);
-        
+
         JLabel Error = new JLabel("  Las dimensiones de las imágenes deben ser iguales.");
         JButton Aceptar = new JButton("Aceptar");
 
@@ -65,22 +66,22 @@ public class Diferencia_dos_imagenes {
 
         error.setVisible(true);
     }
-    
-    private void elegirImagenDos(){
+
+    private void elegirImagenDos() {
         final JFrame elegir = new JFrame();
         elegir.setTitle("Elige imagen para comparar");
         elegir.setSize(300, 150);
         elegir.setResizable(false);
         elegir.setLocationRelativeTo(null);
-        
+
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3, 1));
         elegir.add(panel);
-        
+
         JLabel Imagen = new JLabel("Elige la segunda imagen:");
         final JSpinner Dat_imagen = new JSpinner();
         JButton Aceptar = new JButton("Aceptar");
-        
+
         Aceptar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
                 int i = (int) Dat_imagen.getValue();
@@ -88,13 +89,13 @@ public class Diferencia_dos_imagenes {
                 img2 = gestor_img.getBufferedImage(i);
 //                guardarImg1();
 //                guardarImg2();
-                
-                if(comprobarDimensiones()){
-         
+
+                if (comprobarDimensiones()) {
+                    gestor_img.anadirImagen(mapaDeCambios());
                 } else {
                     ventanaError();
                 }
-                
+
             }
         });
 
@@ -104,25 +105,75 @@ public class Diferencia_dos_imagenes {
 
         elegir.setVisible(true);
     }
-    
-    private void guardarImg1(){
+
+    private void guardarImg1() {
         BufferedImage r1 = new BufferedImage(img1.getWidth(), img1.getHeight(), img1.getType());
-        for(int i = 0; i < img1.getWidth(); i++){
-            for(int j = 0; j < img1.getHeight(); j++){
+        for (int i = 0; i < img1.getWidth(); i++) {
+            for (int j = 0; j < img1.getHeight(); j++) {
                 r1.setRGB(i, j, img1.getRGB(i, j));
             }
         }
         gestor_img.anadirImagen(r1);
     }
-    
-    private void guardarImg2(){
+
+    private void guardarImg2() {
         BufferedImage r2 = new BufferedImage(img2.getWidth(), img2.getHeight(), img2.getType());
-        for(int i = 0; i < img2.getWidth(); i++){
-            for(int j = 0; j < img2.getHeight(); j++){
+        for (int i = 0; i < img2.getWidth(); i++) {
+            for (int j = 0; j < img2.getHeight(); j++) {
                 r2.setRGB(i, j, img2.getRGB(i, j));
             }
         }
         gestor_img.anadirImagen(r2);
+    }
+
+    private BufferedImage resta() {
+        BufferedImage resultado = new BufferedImage(img1.getWidth(), img1.getHeight(), img1.getType());
+
+        int tmp = 0;
+        for (int i = 0; i < img1.getWidth(); i++) {
+            for (int j = 0; j < img1.getHeight(); j++) {
+                tmp = Math.abs(new Color(img1.getRGB(i, j)).getRed() - new Color(img2.getRGB(i, j)).getRed());
+                resultado.setRGB(i, j, new Color(tmp, tmp, tmp).getRGB());
+            }
+        }
+
+        return resultado;
+    }
+
+    public HashMap<Integer, Integer> tablaTransformacion(int uCambio) {
+        // Si el valor es mayor que cambio entonces -1
+        HashMap<Integer, Integer> tabla = new HashMap<>();
+        int tmp = 0;
+        for (int i = 0; i < 256; i++) {
+            if (i > uCambio) {
+                tmp = -1;
+            } else {
+                tmp = i;
+            }
+            tabla.put(i, tmp);
+        }
+        return tabla;
+    }
+
+    private BufferedImage mapaDeCambios() {
+        BufferedImage resultado = new BufferedImage(img1.getWidth(), img1.getHeight(), img1.getType());
+
+//        HashMap<Integer, Integer> tabla = tablaTransformacion(uCambio);
+        HashMap<Integer, Integer> tabla = tablaTransformacion(10);
+        int tmp = 0;
+        for (int i = 0; i < img1.getWidth(); i++) {
+            for (int j = 0; j < img1.getHeight(); j++) {
+                tmp = tabla.get(Math.abs(new Color(img1.getRGB(i, j)).getRed() - new Color(img2.getRGB(i, j)).getRed()));
+                if (tmp == -1) {
+                    resultado.setRGB(i, j, Color.RED.getRGB()); // Cambia
+
+                } else {
+                    resultado.setRGB(i, j, img1.getRGB(i, j)); // No Cambia
+                }
+            }
+        }
+
+        return resultado;
     }
 
 }
