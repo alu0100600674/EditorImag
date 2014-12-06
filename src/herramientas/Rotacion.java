62d03fc09b8e03c41b77ddcd3eed63146062067f
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-12-06 18:54:08 +0000

    Pidiendo datos de rotación por pantalla

--- src/herramientas/Rotacion.java
+++ src/herramientas/Rotacion.java
@@ -6,7 +6,15 @@
 
 package herramientas;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -15,6 +23,7 @@ import java.awt.image.BufferedImage;
 public class Rotacion {
     
     private BufferedImage img;
+    private int rotacion;
     
     public Rotacion(BufferedImage tmp){
         img = tmp;
@@ -57,7 +66,53 @@ public class Rotacion {
     }
     
     public BufferedImage rotar(){
+        controles();
+        
+        if(rotacion >= 360){ //Si la rotación es mayor a 360º, convertir a una vuelta.
+            rotacion -= 360;
+        }
+        
+        if((rotacion != 90) && (rotacion != 180) && (rotacion != 270) && (rotacion != 0)){
+            
+        }
+        else{
+            
+        }
+        
         return null; //Cambiar cuando se haga este método.
+    }
+    
+    private void controles(){
+        final JFrame v = new JFrame();
+        v.setTitle("Rotar");
+        v.setSize(300, 150);
+        v.setResizable(false);
+        v.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 1));
+        v.add(panel);
+
+        JLabel Rotar = new JLabel(" Introduce la rotación");
+//        final JSpinner Dat_gamma = new JSpinner();
+        
+        final JTextField Dat_rotar = new JTextField();
+        
+        JButton Aceptar = new JButton("Aceptar");
+
+        Aceptar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                rotacion = Integer.parseInt(Dat_rotar.getText());
+                v.dispose();
+            }
+        });
+
+        panel.add(Rotar);
+        panel.add(Dat_rotar);
+        panel.add(Aceptar);
+
+        v.setVisible(true);
+        
     }
     
 }
