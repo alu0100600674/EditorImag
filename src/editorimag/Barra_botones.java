2a9f16c53c36fd9544cf10afb1212a9d1ec5d1c9
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-12-07 22:09:08 +0000

    Añadidas imágenes a los botones

--- src/editorimag/Barra_botones.java
+++ src/editorimag/Barra_botones.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package editorimag;
 
 import static editorimag.EditorImag.activa;
 import static editorimag.EditorImag.gestor_img;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -20,58 +20,77 @@ import javax.swing.JToolBar;
  *
  * @author jonay
  */
-public class Barra_botones extends JToolBar{
+public class Barra_botones extends JToolBar {
+
     //modificados pero no pasaba nada sin el final
+
     private final Boton Abrir;
     private final Boton Guardar;
-    
+
     private Boton Espejo_H;
     private Boton Espejo_V;
-    
+
     private Boton Rotar_90;
     private Boton Rotar_180;
     private Boton Rotar_270;
-    
+    private Boton Rotar;
+
     //Iconos
-    private ImageIcon abrir = new ImageIcon("src/images/abrir2.png");
-    private ImageIcon guardar = new ImageIcon("src/images/guardar2.png");
-    
-    
-    
-    public Barra_botones(){
+    private int tam = 30;
+    private ImageIcon abrir = new ImageIcon("src/images/abrir.png");
+    private ImageIcon guardar = new ImageIcon("src/images/guardar.png");
+    private ImageIcon espejoH = new ImageIcon("src/images/espejoH.png");
+    private ImageIcon espejoV = new ImageIcon("src/images/espejoV.png");
+    private ImageIcon rotar90 = new ImageIcon("src/images/rotar90.png");
+    private ImageIcon rotar180 = new ImageIcon("src/images/rotar180.png");
+    private ImageIcon rotar270 = new ImageIcon("src/images/rotar270.png");
+    private ImageIcon rotar = new ImageIcon("src/images/rotar.png");
+
+    public Barra_botones() {
+        escalarImagenes();
+
         Abrir = new Boton("Abrir", abrir);
         this.add(Abrir);
         Guardar = new Boton("Guardar", guardar);
         this.add(Guardar);
-        
+
         this.addSeparator();
-        
-        Espejo_H = new Boton("Espejo Horizontal", null);
+
+        Espejo_H = new Boton("Espejo Horizontal", espejoH);
         this.add(Espejo_H);
-        Espejo_V = new Boton("Espejo Vertical", null);
+        Espejo_V = new Boton("Espejo Vertical", espejoV);
         this.add(Espejo_V);
-        
+
         this.addSeparator();
-        
-        Rotar_90 = new Boton("Rotar 90º", null);
+
+        Rotar_90 = new Boton("Rotar 90º", rotar90);
         this.add(Rotar_90);
-        Rotar_180 = new Boton("Rotar 180º", null);
+        Rotar_180 = new Boton("Rotar 180º", rotar180);
         this.add(Rotar_180);
-        Rotar_270 = new Boton("Rotar 270º", null);
+        Rotar_270 = new Boton("Rotar 270º", rotar270);
         this.add(Rotar_270);
-        
+        Rotar = new Boton("Rotar", rotar);
+        this.add(Rotar);
+
         activarBotones();
-        
+
     }
-    
-    private void escalarImagenes(){
-        
+
+    private void escalarImagenes() {
+        abrir = new ImageIcon(abrir.getImage().getScaledInstance(tam, tam, Image.SCALE_DEFAULT));
+        guardar = new ImageIcon(guardar.getImage().getScaledInstance(tam, tam, Image.SCALE_DEFAULT));
+        espejoH = new ImageIcon(espejoH.getImage().getScaledInstance(tam, tam, Image.SCALE_DEFAULT));
+        espejoV = new ImageIcon(espejoV.getImage().getScaledInstance(tam, tam, Image.SCALE_DEFAULT));
+        rotar90 = new ImageIcon(rotar90.getImage().getScaledInstance(tam, tam, Image.SCALE_DEFAULT));
+        rotar180 = new ImageIcon(rotar180.getImage().getScaledInstance(tam, tam, Image.SCALE_DEFAULT));
+        rotar270 = new ImageIcon(rotar270.getImage().getScaledInstance(tam, tam, Image.SCALE_DEFAULT));
+        rotar = new ImageIcon(rotar.getImage().getScaledInstance(tam, tam, Image.SCALE_DEFAULT));
     }
-    
-    private void activarBotones(){
-        
+
+    private void activarBotones() {
+
         Abrir.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 try {
                     gestor_img.nuevaImagen();
                 } catch (IOException ex) {
@@ -79,9 +98,9 @@ public class Barra_botones extends JToolBar{
                 }
             }
         });
-        
+
         Guardar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 try {
                     gestor_img.guardarImagen();
                 } catch (IOException ex) {
@@ -89,37 +108,43 @@ public class Barra_botones extends JToolBar{
                 }
             }
         });
-        
+
         Espejo_H.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 gestor_img.getListaLan().get(activa).lanzar(32);
             }
         });
-        
+
         Espejo_V.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 gestor_img.getListaLan().get(activa).lanzar(31);
             }
         });
-        
+
         Rotar_90.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 gestor_img.getListaLan().get(activa).lanzar(34);
             }
         });
-        
+
         Rotar_180.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 gestor_img.getListaLan().get(activa).lanzar(35);
             }
         });
-        
+
         Rotar_270.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 gestor_img.getListaLan().get(activa).lanzar(36);
             }
         });
         
+        Rotar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                gestor_img.getListaLan().get(activa).lanzar(37);
+            }
+        });
+
     }
-    
+
 }
